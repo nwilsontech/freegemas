3a43691a30dec558bdaf8356a95c0e916940b762
theom3ga <theom3ga@db25a673-a229-e60e-18be-92facb3ca57d>
2011-12-21 00:35:22 +0000

    Dividiendo un poco las cosas del StateGame

--- trunk_sfml/src/GameTimeBoard.cpp
+++ trunk_sfml/src/GameTimeBoard.cpp
@@ -4,6 +4,8 @@
 #include "i18n.h"
 #include "log.h"
 
+#include <cmath>
+
 void GameTimeBoard::loadResources (){
 
     // Load the font for the timeboard
@@ -17,16 +19,15 @@ void GameTimeBoard::loadResources (){
     stHeader = sf::String (_("time left"),
                                  ResMgr -> getFont("media/fNormal.ttf", 33),
                                  33);
-    stHeader.SetPosition (113 - stHeader.GetRect().GetWidth() / 2, 186);
+    stHeader.SetPosition (113 - floor(stHeader.GetRect().GetWidth() / 2), 186);
 
     // Redraw the text with the time
     redraw ("00:00");
 };
 
 void GameTimeBoard::redraw (std::string time){
-    stTime = sf::String(time, ftTime, 61);
-    stTime.SetPosition(192 - stTime.GetRect().GetWidth(), 230);
-    lDEBUG << (197 - stTime.GetRect().GetWidth());
+    stTime = sf::String(time, ftTime, 60);
+    stTime.SetPosition(190 - stTime.GetRect().GetWidth(), 226);
 }
 
 void GameTimeBoard::draw (DrawingQueue& queue){
